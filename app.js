255fc7adad5542e2beeb782354e636098570bfb0
Michael Lawrence <me@mikelawrence.co>
2020-03-30 14:39:40 -0400

    Updating env variable usage.

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const cors = require('cors');
 const fetch = require('node-fetch');
 const history = require('connect-history-api-fallback');
-const scUser = '564051336'; // Decker & Rubin
-//const scUser = '12398941'; // Michael Lawrence
 const podcastRSSUrl = `http://feeds.soundcloud.com/users/soundcloud:users:${scUser}/sounds.rss`;
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
+const soundcloud_oauth = process.env.soundcloud_oauth;
+const soundcloud_userid = process.env.soundcloud_userid;
+const soundcloud_clientid = process.env.soundcloud_clientid;
 
 app.use(cors());
 
@@ -19,15 +19,15 @@ app.get('/p/rss', (req, res) => {
     .then(body => res.send(body));
 });
 
-app.get('/p/json', (req, res) => {
+app.get('/p/json', async (req, res) => {
   res.type('application/json');
 
-  fetch(`https://api-v2.soundcloud.com/users/${scUser}/tracks?representation=&client_id=vSW0IjOgX590JQk5rmZdDXn4yg30QKZA&limit=20&offset=0&linked_partitioning=1&app_version=1563180860&app_locale=en`, {
+  const response = await fetch(`https://api-v2.soundcloud.com/users/${soundcloud_userid}/tracks?representation=&client_id=${soundcloud_clientid}&limit=20&offset=0&linked_partitioning=1&app_version=1563180860&app_locale=en`, {
     'credentials': 'include',
     'headers': {
       'accept': 'application/json, text/javascript, */*; q=0.01',
-      'authorization': `OAuth 1-290036-${scUser}-6784b32fe71fd0`,
-      'sec-ch-ua': 'Google Chrome 75',
+      'authorization': soundcloud_oauth,
+      'sec-ch-ua': 'Google Chrome 80',
       'sec-fetch-dest': 'empty',
       'sec-fetch-mode': 'cors',
       'sec-fetch-site': 'same-site'
@@ -37,9 +37,15 @@ app.get('/p/json', (req, res) => {
     'body': null,
     'method': 'GET',
     'mode': 'cors'
-  })
-    .then(response => response.text())
-    .then(body => res.send(body));
+  });
+
+  console.log(response);
+
+  const body = await response.text();
+
+  console.log(body);
+
+  res.send(body);
 });
 
 app.use(history());
