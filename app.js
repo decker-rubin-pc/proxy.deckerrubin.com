a29f7d4bc06e7f6ce61956c43fda9196e1d188af
Michael Lawrence <me@mikelawrence.co>
2020-06-23 15:27:52 -0400

    Updating JSON feed.

--- app.js
+++ app.js
@@ -1,51 +1,43 @@
+const _ = require('lodash');
 const cors = require('cors');
 const fetch = require('node-fetch');
 const history = require('connect-history-api-fallback');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
-const soundcloud_oauth = process.env.soundcloud_oauth;
-const soundcloud_userid = process.env.soundcloud_userid;
-const soundcloud_clientid = process.env.soundcloud_clientid;
+const soundcloud_userid = process.env.soundcloud_userid || '564051336';
 const podcastRSSUrl = `http://feeds.soundcloud.com/users/soundcloud:users:${soundcloud_userid}/sounds.rss`;
+const { parseString } = require('xml2js');
 
 app.use(cors());
 
-app.get('/p/rss', (req, res) => {
-  res.type('application/xml');
-
-  fetch(podcastRSSUrl)
-    .then(response => response.text())
-    .then(body => res.send(body));
-});
-
 app.get('/p/json', async (req, res) => {
-  res.type('application/json');
+    res.type('application/json');
 
-  const response = await fetch(`https://api-v2.soundcloud.com/users/${soundcloud_userid}/tracks?representation=&client_id=${soundcloud_clientid}&limit=20&offset=0&linked_partitioning=1&app_version=1563180860&app_locale=en`, {
-    'credentials': 'include',
-    'headers': {
-      'accept': 'application/json, text/javascript, */*; q=0.01',
-      'authorization': soundcloud_oauth,
-      'sec-ch-ua': 'Google Chrome 80',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'same-site'
-    },
-    'referrer': 'https://soundcloud.com/',
-    'referrerPolicy': 'origin',
-    'body': null,
-    'method': 'GET',
-    'mode': 'cors'
-  });
+    try {
+        const response = await fetch(podcastRSSUrl);
+        const body = await response.text();
 
-  console.log(response);
+        parseString(body, (err, result) => {
+            const tracks = _
+                .chain(result)
+                .get('rss.channel[0].item')
+                .map(x => ({
+                    id: _.last(_.first(x.link).split('/')),
+                    description: _.first(x.description),
+                    title: _.first(x.title),
+                    created_at: new Date(_.first(x.pubDate)),
+                    public: true,
+                    link: _.first(x.link)
+                }))
+                .value();
 
-  const body = await response.text();
-
-  console.log(body);
-
-  res.send(body);
+            res.send(tracks);
+        });
+    } catch (err) {
+        console.error(err);
+        res.send(err);
+    }
 });
 
 app.use(history());
